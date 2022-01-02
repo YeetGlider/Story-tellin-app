8d54866734fb91ed0ca4f0977304b5c0538b47f8
YeetGlider <72340148+YeetGlider@users.noreply.github.com>
2022-01-02 13:48:41 +0530

    Add files via upload

---
+++ screens/StoryCard.js
@@ -0,0 +1,87 @@
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Platform,
+  StatusBar,
+  Image,
+  Dimensions
+} from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { RFValue } from "react-native-responsive-fontsize";
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+
+let customFonts = {
+  "Bubblegum-Sans": require("../assets/fonts/BubblegumSans-Regular.ttf")
+};
+
+export default class StoryCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fontsLoaded: false
+    };
+  }
+
+  async _loadFontsAsync() {
+    await Font.loadAsync(customFonts);
+    this.setState({ fontsLoaded: true });
+  }
+
+  componentDidMount() {
+    this._loadFontsAsync();
+  }
+
+  render() {
+    if (!this.state.fontsLoaded) {
+      return <AppLoading />;
+    } else {
+      return (
+        <View style={styles.container}>
+          <View style={styles.cardContainer}>
+            <Image
+              source={require("../assets/story_image_1.png")}
+              style={styles.storyImage}
+            ></Image>
+
+            <View style={styles.titleContainer}>
+              <Text style={styles.storyTitleText}>
+                {this.props.story.title}
+              </Text>
+              <Text style={styles.storyAuthorText}>
+                {this.props.story.author}
+              </Text>
+              <Text style={styles.descriptionText}>
+                {this.props.story.description}
+              </Text>
+            </View>
+            <View style={styles.actionContainer}>
+              <View style={styles.likeButton}>
+                <Ionicons name={"heart"} size={RFValue(30)} color={"white"} />
+                <Text style={styles.likeText}>12k</Text>
+              </View>
+            </View>
+          </View>
+        </View>
+      );
+    }
+  }
+  }
+  
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  cardContainer:{
+    margin:RFValue(13),
+    backgroundColor:"#2f345d",
+    borderRadius:"30"
+  },
+});
